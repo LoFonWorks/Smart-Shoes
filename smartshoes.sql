b0fcb108e7dddd03b6492ada944e209cbf32eaa9
Khan6098 <103905789+Khan6098@users.noreply.github.com>
2022-11-26 18:24:43 -0600

    Add files via upload

--- smartshoes.sql
+++ smartshoes.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 19, 2022 at 12:08 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 7.4.29
+-- Generation Time: Nov 27, 2022 at 01:18 AM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,16 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `shoes`
 --
 
 CREATE TABLE `shoes` (
   `id` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `gender` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `age` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `age` varchar(255) DEFAULT NULL,
   `cost` int(11) DEFAULT NULL,
-  `shoeArtifact` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL
+  `shoeArtifact` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -41,20 +54,27 @@ CREATE TABLE `shoes` (
 --
 
 INSERT INTO `shoes` (`id`, `name`, `gender`, `age`, `cost`, `shoeArtifact`) VALUES
-(1, 'Men Sneakers', 'male', 'adult', 100, '/Images/Male Shoes 1.jpg'),
-(2, 'Men Sneakers', 'male', 'adult', 120, '/Images/Male Shoes 2.jpg'),
-(3, 'Dress Shoes', 'male', 'adult', 55, '/Images/Male Shoes 3.jpg'),
-(4, 'Dress Shoes', 'male', 'adult', 50, '/Images/Male Shoes 4.jpg'),
+(1, 'Men Sneakers', 'male', 'adult', 100, '/Images/popular1.jpg'),
+(2, 'Men Sneakers', 'male', 'adult', 120, '/Images/popular2.jpg'),
+(3, 'Dress Shoes', 'male', 'adult', 55, '/Images/popular3.jpg'),
+(4, 'Dress Shoes', 'male', 'adult', 50, '/Images/popular4.jpg'),
 (5, 'High Heels', 'female', 'adult', 200, '/Images/Female Shoes 1.jpg'),
 (6, 'High Heels', 'female', 'adult', 50, '/Images/Female Shoes 2.jpg'),
 (7, 'High Heels', 'female', 'adult', 150, '/Images/Female Shoes 3.jpg'),
 (8, 'High Heels', 'female', 'adult', 300, '/Images/Female Shoes 4.jpg'),
 (9, 'Boy Sneakers', 'male', 'child', 125, '/Images/Child Male 1.jpg'),
-(10, 'Female Sneakers', 'female', 'child', 75, '/Images/Child Feale 1.jpg');
+(10, 'Female Sneakers', 'female', 'child', 75, '/Images/Child Female 1.jpg');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `item_id` (`item_id`);
 
 --
 -- Indexes for table `shoes`
@@ -67,10 +87,26 @@ ALTER TABLE `shoes`
 --
 
 --
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
 -- AUTO_INCREMENT for table `shoes`
 --
 ALTER TABLE `shoes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `shoes` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
